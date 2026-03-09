6cb9d07be3b5bb1ad2d3b957cb28e898d328e69c
Joost Gevaert <jhngevaert@gmail.com>
2026-03-09 17:11:02 +0100

    Add the HelloGrasshopper component / experiment

---
+++ hello-grasshopper/HelloGrasshopperInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace HelloGrasshopper
+{
+  public class HelloGrasshopperInfo : GH_AssemblyInfo
+  {
+    public override string Name => "HelloGrasshopper Info";
+
+    //Return a 24x24 pixel bitmap to represent this GHA library.
+    public override Bitmap Icon => null;
+
+    //Return a short string describing the purpose of this GHA library.
+    public override string Description => "";
+
+    public override Guid Id => new Guid("6630d836-39f6-49b8-8640-a3b6d5ab8a7c");
+
+    //Return a string identifying you or your company.
+    public override string AuthorName => "";
+
+    //Return a string representing your preferred contact details.
+    public override string AuthorContact => "";
+
+    //Return a string representing the version.  This returns the same version as the assembly.
+    public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+  }
+}
---
+++ hello-grasshopper/README.md
@@ -0,0 +1,7 @@
+# hello-grasshopper
+
+Developing Rhino/Grasshopper components from VSCode
+
+Rhino's docs recommend to develop Grasshopper components using Visual Studio on Windows. However, the MacOS guide does show how to develop a Grasshopper component from VS Code:
+
+https://developer.rhino3d.com/guides/grasshopper/your-first-component-mac
